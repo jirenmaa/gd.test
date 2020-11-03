3000925144ea892d1216b9add311bf400947e1dc
jirenmaa <jirenmaa@gmail.com>
2020-11-03 22:37:43 +0700

    adding new level and enchange ui

---
+++ src/UserInterface/PlayButton.gd
@@ -0,0 +1,12 @@
+tool
+extends Button
+
+
+export(String, FILE) var next_scene_path: = ""
+
+func _on_button_up() -> void:
+	get_tree().change_scene(next_scene_path)
+
+
+func _get_configuration_warning() -> String:
+	return "must be returning a next scene path" if next_scene_path == "" else ""
